484b175440099b6fff75d578ad78af8b5dc1be1f
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2025-07-11 13:36:39 +0200

    chore: Disable dead code warnings for now

--- proton-rpgp/src/lib.rs
+++ proton-rpgp/src/lib.rs
@@ -1,5 +1,8 @@
-mod decryption;
-mod encryption;
+// Allow dead code for now.
+// TODO: Remove this once we have a proper API.
+#![allow(dead_code)]
+// mod decryption;
+// mod encryption;
 mod errors;
 mod key;
 mod profile;
@@ -12,8 +15,8 @@ pub mod armor;
 /// Re-export the `pgp` crate.
 pub use pgp;
 
-pub use decryption::*;
-pub use encryption::*;
+// pub use decryption::*;
+// pub use encryption::*;
 pub use errors::*;
 pub use key::*;
 pub use profile::*;
