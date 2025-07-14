ce3d4d4a73a1ccbb59be7ad8dc670971a32acbf1
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2025-07-14 14:32:03 +0200

    test(CRYPTO-275): Add RSA 1023-bit sign test

--- proton-rpgp/tests/sign.rs
+++ proton-rpgp/tests/sign.rs
@@ -1,4 +1,4 @@
-use proton_rpgp::{AsPublicKeyRef, DataEncoding, PrivateKey, Signer, UnixTime, Verifier};
+use proton_rpgp::{AsPublicKeyRef, DataEncoding, PrivateKey, Profile, Signer, UnixTime, Verifier};
 
 pub const TEST_KEY: &str = include_str!("../test-data/keys/private_key_v4.asc");
 
@@ -82,3 +82,56 @@ pub fn sign_create_detached_signature_multi() {
 
     assert!(verification_result.is_ok());
 }
+
+#[test]
+#[allow(clippy::missing_panics_doc)]
+pub fn sign_create_detached_signature_rsa_1023() {
+    const TEST_KEY_V4: &str = include_str!("../test-data/keys/locket_private_key_v4_rsa_1023.asc");
+    let date = UnixTime::new(1_752_476_259);
+    let input_data = b"hello world";
+
+    let mut profile = Profile::new();
+    profile.min_rsa_bits = 1023;
+
+    let key = PrivateKey::import(TEST_KEY_V4.as_bytes(), b"password", DataEncoding::Armor)
+        .expect("Failed to import key");
+
+    let signature_bytes = Signer::new(&profile)
+        .with_signing_key(&key)
+        .at_date(date)
+        .sign_detached(input_data, DataEncoding::Armor)
+        .expect("Failed to sign");
+
+    let verification_result = Verifier::new(&profile)
+        .with_verification_key(key.as_public_key())
+        .at_date(date)
+        .verify_detached(input_data, &signature_bytes, DataEncoding::Armor);
+
+    assert!(verification_result.is_ok());
+}
+
+// TODO: Update rpgp to accept ml-dsa as a valid signature algorithm.
+/*#[test]
+#[allow(clippy::missing_panics_doc)]
+pub fn sign_create_detached_signature_v6_pqc() {
+    const KEY: &str = include_str!("../test-data/keys/private_key_v6_pqc.asc");
+    let input_data = b"hello world";
+
+    let date = UnixTime::new(1_752_237_138);
+
+    let key = PrivateKey::import_unlocked(KEY.as_bytes(), DataEncoding::Armor)
+        .expect("Failed to import key");
+
+    let signature_bytes = Signer::default()
+        .with_signing_key(&key)
+        .at_date(date)
+        .sign_detached(input_data, DataEncoding::Armor)
+        .expect("Failed to sign");
+
+    let verification_result = Verifier::default()
+        .with_verification_key(key.as_public_key())
+        .at_date(date)
+        .verify_detached(input_data, &signature_bytes, DataEncoding::Armor);
+
+    assert!(verification_result.is_ok());
+}*/
