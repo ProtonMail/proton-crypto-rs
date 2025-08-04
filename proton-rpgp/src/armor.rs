b07f0774a2e423315250dfee4eb3c11f16488c7b
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2025-08-04 14:36:07 +0200

    feat(CRYPTO-270): Add standalone armor API

--- proton-rpgp/src/armor.rs
+++ proton-rpgp/src/armor.rs
@@ -1,11 +1,127 @@
-use std::io::Read;
+use std::io::{self, Read};
 
 use pgp::{
-    armor::{BlockType, Dearmor},
+    armor::{self, BlockType, Dearmor},
     composed::Message,
+    ser::Serialize,
 };
 
 use crate::{ArmorError, DataEncoding, MessageProcessingError};
+
+const INCLUDE_CHECKSUM: bool = true;
+
+/// Armors a public key.
+///
+/// Prodcues a message of the form:
+/// ```skip
+/// -----BEGIN PGP PUBLIC KEY BLOCK-----
+///
+/// ...
+/// -----END PGP PUBLIC KEY BLOCK-----
+/// ```
+pub fn armor_public_key(public_key: impl AsRef<[u8]>) -> Result<Vec<u8>, ArmorError> {
+    let mut armored = Vec::with_capacity(public_key.as_ref().len());
+    encode_to_buffer(
+        &BinaryArmorSource {
+            data: public_key.as_ref(),
+        },
+        BlockType::PublicKey,
+        INCLUDE_CHECKSUM,
+        &mut armored,
+    )?;
+    Ok(armored)
+}
+
+/// Armors a private key.
+///
+/// Prodcues a message of the form:
+/// ```skip
+/// -----BEGIN PGP PRIVATE KEY BLOCK-----
+///
+/// ...
+/// -----END PGP PRIVATE KEY BLOCK-----
+/// ```
+pub fn armor_private_key(private_key: impl AsRef<[u8]>) -> Result<Vec<u8>, ArmorError> {
+    let mut armored = Vec::with_capacity(private_key.as_ref().len());
+    encode_to_buffer(
+        &BinaryArmorSource {
+            data: private_key.as_ref(),
+        },
+        BlockType::PrivateKey,
+        INCLUDE_CHECKSUM,
+        &mut armored,
+    )?;
+    Ok(armored)
+}
+
+/// Armors a signature.
+///
+/// Prodcues a message of the form:
+/// ```skip
+/// -----BEGIN PGP SIGNATURE-----
+///
+/// ...
+/// -----END PGP SIGNATURE-----
+/// ```
+pub fn armor_signature(signature: impl AsRef<[u8]>) -> Result<Vec<u8>, ArmorError> {
+    let mut armored = Vec::with_capacity(signature.as_ref().len());
+    encode_to_buffer(
+        &BinaryArmorSource {
+            data: signature.as_ref(),
+        },
+        BlockType::Signature,
+        INCLUDE_CHECKSUM,
+        &mut armored,
+    )?;
+    Ok(armored)
+}
+
+/// Armors a message.
+///
+/// Prodcues a message of the form:
+/// ```skip
+/// -----BEGIN PGP MESSAGE-----
+///
+/// ...
+/// -----END PGP MESSAGE-----
+/// ```
+pub fn armor_message(message: impl AsRef<[u8]>) -> Result<Vec<u8>, ArmorError> {
+    let mut armored = Vec::with_capacity(message.as_ref().len());
+    encode_to_buffer(
+        &BinaryArmorSource {
+            data: message.as_ref(),
+        },
+        BlockType::Message,
+        INCLUDE_CHECKSUM,
+        &mut armored,
+    )?;
+    Ok(armored)
+}
+
+/// Unarmors the input.
+///
+/// The input must have the form:
+/// ```skip
+/// -----BEGIN PGP <TYPE>-----
+///
+/// ...
+/// -----END PGP <TYPE>-----
+/// ```
+pub fn unarmor(armored: impl AsRef<[u8]>) -> Result<Vec<u8>, ArmorError> {
+    let mut output = Vec::with_capacity(armored.as_ref().len());
+    decode_to_buffer(armored.as_ref(), None, &mut output)?;
+    Ok(output)
+}
+
+pub(crate) fn encode_to_buffer(
+    input: &BinaryArmorSource<'_>,
+    block_type: BlockType,
+    include_checksum: bool,
+    mut output: impl io::Write,
+) -> Result<(), ArmorError> {
+    armor::write(&input, block_type, &mut output, None, include_checksum)
+        .map_err(ArmorError::Encode)
+}
 
 /// Unarmor the input into the output buffer.
 ///
@@ -44,5 +160,20 @@ pub(crate) fn decode_to_message(
         DataEncoding::Unarmored => {
             Message::from_bytes(input).map_err(MessageProcessingError::MessageParsing)
         }
+    }
+}
+
+pub(crate) struct BinaryArmorSource<'a> {
+    data: &'a [u8],
+}
+
+impl Serialize for BinaryArmorSource<'_> {
+    fn to_writer<W: io::Write>(&self, writer: &mut W) -> pgp::errors::Result<()> {
+        writer.write_all(self.data)?;
+        Ok(())
+    }
+
+    fn write_len(&self) -> usize {
+        self.data.len()
     }
 }
---
+++ proton-rpgp/tests/armor.rs
@@ -0,0 +1,65 @@
+use proton_rpgp::{
+    armor::{armor_message, armor_private_key, armor_public_key, armor_signature, unarmor},
+    ArmorError,
+};
+
+const TEST_PUBLIC_KEY: &[u8] = b"dummy public key bytes";
+const TEST_PRIVATE_KEY: &[u8] = b"dummy private key bytes";
+const TEST_SIGNATURE: &[u8] = b"dummy signature bytes";
+const TEST_MESSAGE: &[u8] = b"dummy message bytes";
+
+fn is_armored_with_header(armored: &[u8], header: &str) -> bool {
+    let armored_str = std::str::from_utf8(armored).unwrap();
+    armored_str.starts_with(header)
+}
+
+#[test]
+fn test_armor_public_key_and_unarmor() {
+    let armored = armor_public_key(TEST_PUBLIC_KEY).expect("armor_public_key failed");
+    assert!(
+        is_armored_with_header(&armored, "-----BEGIN PGP PUBLIC KEY BLOCK-----"),
+        "Missing public key armor header"
+    );
+    let unarmored = unarmor(&armored).expect("unarmor failed");
+    assert_eq!(unarmored, TEST_PUBLIC_KEY);
+}
+
+#[test]
+fn test_armor_private_key_and_unarmor() {
+    let armored = armor_private_key(TEST_PRIVATE_KEY).expect("armor_private_key failed");
+    assert!(
+        is_armored_with_header(&armored, "-----BEGIN PGP PRIVATE KEY BLOCK-----"),
+        "Missing private key armor header"
+    );
+    let unarmored = unarmor(&armored).expect("unarmor failed");
+    assert_eq!(unarmored, TEST_PRIVATE_KEY);
+}
+
+#[test]
+fn test_armor_signature_and_unarmor() {
+    let armored = armor_signature(TEST_SIGNATURE).expect("armor_signature failed");
+    assert!(
+        is_armored_with_header(&armored, "-----BEGIN PGP SIGNATURE-----"),
+        "Missing signature armor header"
+    );
+    let unarmored = unarmor(&armored).expect("unarmor failed");
+    assert_eq!(unarmored, TEST_SIGNATURE);
+}
+
+#[test]
+fn test_armor_message_and_unarmor() {
+    let armored = armor_message(TEST_MESSAGE).expect("armor_message failed");
+    assert!(
+        is_armored_with_header(&armored, "-----BEGIN PGP MESSAGE-----"),
+        "Missing message armor header"
+    );
+    let unarmored = unarmor(&armored).expect("unarmor failed");
+    assert_eq!(unarmored, TEST_MESSAGE);
+}
+
+#[test]
+fn test_unarmor_invalid_input() {
+    let invalid = b"not an armored message";
+    let result = unarmor(invalid);
+    assert!(matches!(result, Err(ArmorError::DecodeHeader)));
+}
