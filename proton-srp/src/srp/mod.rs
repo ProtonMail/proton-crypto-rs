f3fe32f6c780bd2c6b4e531b92ec94f839a92ef9
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2025-11-12 11:00:33 +0100

    feat(CRYPTO-332): Allow to use custom random number generator in SRP

--- proton-srp/src/srp/mod.rs
+++ proton-srp/src/srp/mod.rs
@@ -9,8 +9,13 @@ mod server;
 pub use server::*;
 mod client;
 pub use client::*;
+use rand::{CryptoRng, Rng};
 
 pub use core::{SALT_LEN_BYTES, SRP_LEN_BYTES};
 
 #[cfg(test)]
 use core::TEST_CLIENT_SECRET_LEN;
+
+pub(crate) fn srp_default_csprng() -> impl Rng + CryptoRng {
+    rand::thread_rng()
+}
